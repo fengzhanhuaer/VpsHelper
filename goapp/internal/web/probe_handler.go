51ba23f2ff86c0dc021436e9ba989b6f0ee092f8
fengzhanhuaer <fengzhanhuaer@163.com>
2026-02-25 10:19:49 +0800

    优化名称

--- goapp/internal/web/probe_handler.go
+++ goapp/internal/web/probe_handler.go
@@ -438,7 +438,7 @@ func (h *Handler) probePingDashboard(c *gin.Context) {
 	tasks, _ := store.ListProbeTasks(h.dbConn)
 
 	c.HTML(http.StatusOK, "probe_ping_dashboard.html", gin.H{
-		"Title": "拨测监控",
+		"Title": "网络状态",
 		"Nodes": nodes,
 		"Tasks": tasks,
 	})
